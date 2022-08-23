06fb0f263994c52c822f662f513564088704f226
rene dlean <renedlean@hotmail.com>
2022-08-23 19:36:01 -0300

    ex4

--- FUNDAMENTOS-C/aula4/ex4.c
+++ FUNDAMENTOS-C/aula4/ex4.c
@@ -4,6 +4,6 @@ main(void) {
  float far, cel;   
  printf("Informe os graus em Celcius:\n");
  scanf("%f",&cel);
- far = (cel*1,8) + 32;
+ far = ((cel*1.8) + 32);
  printf ("A temperatura em Fahrenheit é %f\n",far);
 }
