69990e8a0bd78f66f82ee61a75008e7f849997e8
rene dlean <renedlean@hotmail.com>
2022-08-23 19:53:05 -0300

    ex6

---
+++ FUNDAMENTOS-C/aula4/ex6.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+main() {
+  
+ float raio, result; 
+
+ printf("Informe o Raio do circulo: \n");
+ scanf("%f",&raio);
+
+ result = (3.14 * (raio*raio));
+
+ printf ("A area do circulo e: %f \n",result);
+
+}
